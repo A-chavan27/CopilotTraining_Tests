cbe34828ac3ebd607b1384b2174dede8f8a51223
A-chavan27 <Chavanaishwarya2797@gmail.com>
2025-02-24 07:05:05 +0000

    Copilot third commit-2

--- comments.js
+++ comments.js
@@ -1,20 +1,8 @@
-// create web server
-// create express app
-// create a router
-// create a route
-// create a callback function
-// send a response
-
-const express = require('express');
-const app = express();
-const commentsRouter = express.Router();
-
-commentsRouter.get('/', (req, res) => {
-  res.send('Comments');
-});
-
-app.use('/comments', commentsRouter);
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+// create add number function
+// add a number to a comment
+// return the new number of the comment
+// return the new number of the comment
+function addNumber(comment) {
+  comment.number = comment.number + 1;
+  return comment.number;
+}
