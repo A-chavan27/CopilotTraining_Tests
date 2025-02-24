3e4c4d8f47b3599a1399e759ffd0eba44702feac
A-chavan27 <Chavanaishwarya2797@gmail.com>
2025-02-24 07:11:15 +0000

    Copilot third commit-3

--- comments.js
+++ comments.js
@@ -1,8 +1,23 @@
-// create add number function
-// add a number to a comment
-// return the new number of the comment
-// return the new number of the comment
-function addNumber(comment) {
-  comment.number = comment.number + 1;
-  return comment.number;
-}
+// create web server
+var express = require('express');
+var app = express();
+// create a router
+var router = express.Router();
+// create a comments array
+var comments = [];
+// add comments to the array
+comments.push({name: 'John', message: 'Hello!'});
+comments.push({name: 'Mary', message: 'Hi!'});
+// create a route for the comments
+router.get('/', function(req, res) {
+  res.json(comments);
+});
+// add the route to the express app
+app.use('/comments', router);
+// start the server
+var port = 3000;
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
+// test the server
+// open the browser and go to http://localhost:3000/comments
+// you should see the comments in JSON format
